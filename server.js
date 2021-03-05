cfeb550bc445f341d1b3b68387d8c9d7ddc98036
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-03-05 11:40:52 +0200

    setup the data we want from the api url and connect it with the constructor function

--- server.js
+++ server.js
@@ -27,20 +27,46 @@ const client = new pg.Client(process.env.DATABASE_URL);
 // 	ssl: { rejectUnauthorized: false },
 // });
 
+//routes
+app.get('/', (req, res) => {
+	let url = 'https://api.pokemontcg.io/v2/cards?page=1&pageSize=20';
+	superagent.get(url).then((results) => {
+		let { data } = results.body;
+		let pokemons = data.map((item) => {
+			return new Pokemon(item);
+		});
+		res.send(pokemons);
+	});
+});
+
 //constructor
 function Pokemon(data) {
-	this.name = data;
-	this.description = data;
-	this.level = data;
-	this.hp = data;
-	this.type = data;
-	this.abilities = data;
-	this.image = data;
-	this.attack = data;
-	this.defence = data;
-	this.weaknesses = data;
-	this.evolvesFrom = data;
-	this.evolvesTo = data;
+	this.name = data.name;
+	this.description = data.flavorText
+		? data.flavorText
+		: 'No description for this Pokemon becuase it is rare';
+	this.level = data.level;
+	this.hp = data.hp;
+	this.types = data.types;
+	this.abilities = data.abilities
+		? data.abilities.map((ability) => ability.name)
+		: 'There are no abilities for this pokemon';
+	this.image = data.images.large || data.images.small;
+	this.attacks = data.attacks
+		? data.attacks.map((attack) => attack.name)
+		: 'This pokemon has no attacks abilities';
+	this.defence = data.resistances
+		? data.resistances.map((resistance) => resistance.type)
+		: 'This pokemon has no defences abilities';
+	this.weaknesses = data.weaknesses
+		? data.weaknesses.map((weakness) => weakness.type)
+		: 'This pokemon has no weaknesses';
+	this.evolvesFrom = data.evolvesFrom
+		? data.evolvesFrom
+		: 'First Version of This kind';
+	this.evolvesTo = data.evolvesTo
+		? data.evolvesTo
+		: 'Last Version of This kind';
 }
 
 // listen
