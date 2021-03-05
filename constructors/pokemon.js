b931fe4156bb5a2526974ac0e21349f4f673ba39
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-03-05 15:35:22 +0200

    create homeHandler to use it

---
+++ constructors/pokemon.js
@@ -0,0 +1,31 @@
+//constructor
+function Pokemon(data) {
+	this.name = data.name;
+	this.description = data.flavorText
+		? data.flavorText
+		: 'No description for this Pokemon becuase it is rare';
+	this.level = data.level;
+	this.hp = parseInt(data.hp);
+	this.types = data.types;
+	this.abilities = data.abilities
+		? data.abilities.map((ability) => ability.name).join(' ')
+		: 'There are no abilities for this pokemon';
+	this.image = data.images.large || data.images.small;
+	this.attacks = data.attacks
+		? data.attacks.map((attack) => attack.name).join(' ')
+		: 'This pokemon has no attacks abilities';
+	this.defence = data.resistances
+		? data.resistances.map((resistance) => resistance.type).join(' ')
+		: 'This pokemon has no defences abilities';
+	this.weaknesses = data.weaknesses
+		? data.weaknesses.map((weakness) => weakness.type).join(' ')
+		: 'This pokemon has no weaknesses';
+	this.evolvesFrom = data.evolvesFrom
+		? data.evolvesFrom
+		: 'First Version of This kind';
+	this.evolvesTo = data.evolvesTo
+		? data.evolvesTo
+		: 'Last Version of This kind';
+}
+
+module.exports = Pokemon;
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 'use strict';
 
-//libraries
+//npm packages
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const superagent = require('superagent');
-const pg = require('pg');
 const methodOverride = require('method-override');
+
+//database file
+const client = require('./data/database');
+
+//handlers
+const homeHandler = require('./handlers/homeHandler');
 
 //app setup
 const app = express();
@@ -18,56 +23,10 @@ app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-//database setup locally
-const client = new pg.Client(process.env.DATABASE_URL);
-
-//database setup for heroku
-// const client = new pg.Client({
-// 	connectionString: process.env.DATABASE_URL,
-// 	ssl: { rejectUnauthorized: false },
-// });
+//let url = `https://api.pokemontcg.io/v2/cards?page=1&pageSize=20`;
 
 //routes
-app.get('/', (req, res) => {
-	let url = 'https://api.pokemontcg.io/v2/cards?page=1&pageSize=20';
-	superagent.get(url).then((results) => {
-		let { data } = results.body;
-		let pokemons = data.map((item) => {
-			return new Pokemon(item);
-		});
-		res.send(pokemons);
-	});
-});
-
-//constructor
-function Pokemon(data) {
-	this.name = data.name;
-	this.description = data.flavorText
-		? data.flavorText
-		: 'No description for this Pokemon becuase it is rare';
-	this.level = data.level;
-	this.hp = data.hp;
-	this.types = data.types;
-	this.abilities = data.abilities
-		? data.abilities.map((ability) => ability.name)
-		: 'There are no abilities for this pokemon';
-	this.image = data.images.large || data.images.small;
-	this.attacks = data.attacks
-		? data.attacks.map((attack) => attack.name)
-		: 'This pokemon has no attacks abilities';
-	this.defence = data.resistances
-		? data.resistances.map((resistance) => resistance.type)
-		: 'This pokemon has no defences abilities';
-	this.weaknesses = data.weaknesses
-		? data.weaknesses.map((weakness) => weakness.type)
-		: 'This pokemon has no weaknesses';
-	this.evolvesFrom = data.evolvesFrom
-		? data.evolvesFrom
-		: 'First Version of This kind';
-	this.evolvesTo = data.evolvesTo
-		? data.evolvesTo
-		: 'Last Version of This kind';
-}
+app.get('/home', homeHandler);
 
 // listen
 const PORT = process.env.PORT || 3000;
