76c8811a639f4cb1749f0503e1e5bb4297fe5dca
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-03-06 20:47:35 +0200

    competition page order

---
+++ handlers/loginHandler.js
@@ -0,0 +1,5 @@
+const client = require('./../data/database');
+
+const loginHandler = (req, res) => {};
+
+module.exports = loginHandler;
--- server.js
+++ server.js
@@ -1,44 +1,48 @@
-"use strict";
+'use strict';
 
 //npm packages
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-const superagent = require("superagent");
-const methodOverride = require("method-override");
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const methodOverride = require('method-override');
 
 //database file
-const client = require("./data/database");
+const client = require('./data/database');
 
-//handlers
-const homeHandler = require("./handlers/homeHandler");
-const dashboardHandler = require("./handlers/dashboardHandler");
-const charactersHandler = require("./handlers/charactersHandler");
-const rockPaperHandler = require("./handlers/rockPaperHandler");
-const descriptionHandler = require("./handlers/descriptionHandler");
+//handlers functions
+const homeHandler = require('./handlers/homeHandler');
+const dashboardHandler = require('./handlers/dashboardHandler');
+const charactersHandler = require('./handlers/charactersHandler');
+const rockPaperHandler = require('./handlers/rockPaperHandler');
+const descriptionHandler = require('./handlers/descriptionHandler');
+const competitionHandler = require('./handlers/competitionHandler');
+const loginHandler = require('./handlers/loginHandler');
+const signUpHandler = require('./handlers/signUpHandler');
+
 
 //app setup
 const app = express();
 
 //middlewares
 app.use(cors());
-app.use(methodOverride("_method"));
-app.use(express.static("./public"));
+app.use(methodOverride('_method'));
+app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 //routes
-app.get("/home", homeHandler);
-app.get("/characters", charactersHandler);
-app.get("/rockPaperSissiors/:trainer", rockPaperHandler);
-app.get("/dashboard", dashboardHandler);
-app.get("/description/:pokemonId", descriptionHandler);
+app.get('/home', homeHandler);
+app.get('/dashboard', dashboardHandler);
+app.get('/description/:pokemonId', descriptionHandler);
+app.get('/characters', charactersHandler);
+app.get('/rockPaperSissiors/:trainer', rockPaperHandler);
+app.get('/competition', competitionHandler);
 
 // listen
 const PORT = process.env.PORT || 3000;
 
 client.connect().then(() => {
-  app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
-  });
+	app.listen(PORT, () => {
+		console.log(`http://localhost:${PORT}`);
+	});
 });
