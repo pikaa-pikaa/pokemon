85e57ff3584e4dd4061dccf90d0495bd62b9d2ce
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-03-09 11:42:08 +0200

    ssl

--- data/database.js
+++ data/database.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 const pg = require('pg');
 
 //database setup locally
-const client = new pg.Client(process.env.DATABASE_URL);
+// const client = new pg.Client(process.env.DATABASE_URL);
 
-//database setup for heroku
-// const client = new pg.Client({
-// 	connectionString: process.env.DATABASE_URL,
-// 	ssl: { rejectUnauthorized: false },
-// });
+// database setup for heroku
+const client = new pg.Client({
+	connectionString: process.env.DATABASE_URL,
+	ssl: { rejectUnauthorized: false },
+});
 
 module.exports = client;
